95811411e73ddad9163777e9101609542caa43fd
cynienaputri <cahyani.adina7268@student.unri.ac.id>
2026-02-26 03:41:59 +0700

    Add files via upload

---
+++ pertemuan-4/latihan-4/test.py
@@ -0,0 +1,18 @@
+#contoh konversi tipe data menggunakann integer/float
+angka = int(input("Masukkan angka: "))
+
+#menangani exception 
+try:
+    # kode yang berpotensi error
+except:
+    # kode penanganan error
+
+#struktur lengkapnya
+try:
+    ...
+except ValueError:
+    ...
+else:
+    ...
+finally:
+    ...
